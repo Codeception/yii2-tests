f32dc80f0e9cbe573c2cf31df6b44781b3f203f7
Sam Mousa <sam@mousa.nl>
2018-03-26 15:33:24 +0200

    Improved tests, enabled debugging

--- cases/sqlite/functional/SqLiteCest.php
+++ cases/sqlite/functional/SqLiteCest.php
@@ -23,22 +23,23 @@ class SqLiteCest
     {
         /** @var Connection $db1 */
         $db1 = $I->grabComponent('db1');
-        $I->assertEquals(['test'], $db1->schema->getTableNames());
+        $I->assertEquals(['test'], $db1->schema->getTableNames('', true));
 
         /** @var Connection $db21 */
         $db21 = $I->grabComponent('db21');
-        $I->assertEquals(['test'], $db21->schema->getTableNames());
+        $I->assertEquals(['test'], $db21->schema->getTableNames('', true));
 
         /** @var Connection $db22 */
         $db22 = $I->grabComponent('db22');
 
-        $I->assertEquals(['test'], $db22->schema->getTableNames());
+        $I->assertEquals(['test'], $db22->schema->getTableNames('', true));
     }
 
     public function testTransaction(FunctionalTester $I)
     {
         /** @var Connection $db1 */
         $db1 = $I->grabComponent('db1');
+        $I->assertFalse($db1->isActive);
         $db1->open();
         $I->assertNotNull($db1->getTransaction());
     }
